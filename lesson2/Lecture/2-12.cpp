49465cc865c16fe74198f08acf0be93b227bee41
CHENYI <r07922059@ntu.edu.tw>
2019-07-12 17:16:26 +0800

    新增 lesson2 上課資料

---
+++ lesson2/Lecture/2-12.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int sum = 0;
+	for (int i = 0 ; i < 1000 ; i = i + 1){ // 計數器
+		//sum = sum + (i+1);
+		//if ( ((i+1) % 3 == 0) && ((i+1) % 5 == 0) && ((i+1) % 7 == 0)){
+		//	cout << (i+1) << "是3,5,7的倍數" << endl;
+		//}
+		cout << "Hello, world!" << endl;
+	}
+
+	cout << sum << endl;
+
+	/*
+	// i++  :  i = i + 1
+	for (int j = 0 ; j < 10 ; j++){
+		cout << j << endl;
+		j = j + 1;
+	}*/
+
+
+	return 0;
+}
+
+
